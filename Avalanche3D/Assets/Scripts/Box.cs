456f5548521d2993640a7e420cb419e135b70741
reinier303 <30725987+reinier303@users.noreply.github.com>
2019-09-02 15:49:55 +0200

    Revert "Revert "Added Project""
    This reverts commit 130074468d8f1b0c83fe9beb82e6a83744d263d6.

---
+++ Avalanche3D/Assets/Scripts/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public bool IsPlaced;
+    public bool CanBeHalted;
+
+    private void Start()
+    {
+        float randomSize = Random.Range(1,3.1f);
+        transform.localScale = new Vector3(randomSize, randomSize, randomSize);
+        rigidBody = GetComponent<Rigidbody>();
+        CanBeHalted = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(CanBeHalted)
+        {           
+            if(collision.gameObject.tag == "Box")
+            {
+                IsPlaced = true;
+                rigidBody.isKinematic = true;
+            }
+        }
+    }
+}
