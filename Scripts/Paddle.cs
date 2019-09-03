b356ac5b8ea83f002efde330a93bbf9d0a741cee
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-09-03 23:23:11 +0200

    re #12, added PaddleInput.cs and Paddle.cs

---
+++ Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	private PaddleInput paddleInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		paddleInput = GetComponent<PaddleInput>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		paddleInput.PaddleMovement();
+    }
+}
---
+++ Scripts/PaddleInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleInput : MonoBehaviour
+{
+	[SerializeField] private float speed = 8;
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void PaddleMovement()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+		rb.velocity = movement * speed;
+	}
+}
