577d1b6db5eb756ce8df5d4cae8dfdf4ee0e067e
DennisBorst <dennis.borst@hotmail.nl>
2019-09-03 23:49:24 +0200

    re #7, I worked on an interface to define the damage

---
+++ Bomberman 3D/Bomberman 3D/Assets/Scripts/DestructableWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableWall : MonoBehaviour, IDamagable
+{
+    public void Damage()
+    {
+        Die();
+    }
+
+    public void Die()
+    {
+
+    }
+}
