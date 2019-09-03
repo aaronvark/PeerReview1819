7c187278b2cc09cec3e2613c6fcab869b2b077c3
DennisBorst <dennis.borst@hotmail.nl>
2019-09-03 13:44:11 +0200

    re #7, I added the project and added a standard unity first person character

---
+++ Bomberman 3D/Bomberman 3D/Assets/Scripts/BomExplode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomExplode : MonoBehaviour
+{
+    [SerializeField] private float explodeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExplodeTime());
+    }
+
+    IEnumerator ExplodeTime()
+    {
+        yield return new WaitForSeconds(explodeTime);
+        Debug.Log("I got destroyed");
+        Destroy(this);
+    }
+}
---
+++ Bomberman 3D/Bomberman 3D/Assets/Scripts/DropBomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBomb : MonoBehaviour
+{
+    public GameObject bomb;
+    public bool bombDeployPossible = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && bombDeployPossible)
+        {
+            bombDeployPossible = false;
+            DeployBomb();
+        }
+    }
+
+    void DeployBomb()
+    {
+        Instantiate(bomb, transform.position, Quaternion.identity);
+    }
+}
