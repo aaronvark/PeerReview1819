e37d6364faa6177c4dc29f1f22523df8b2ae1a39
Ruben Hooijer <loljoch@gmail.com>
2019-09-03 12:10:54 +0200

    re #16, created a block class and coordinate struct to start making a grid

---
+++ Bright Blocks/Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour{
+
+    public Block(Coordinate _coordinate) {
+
+        coordinate = _coordinate;
+    }
+
+    public bool isSet;
+    public Coordinate coordinate;
+
+    private Material material;
+
+    private void Awake() {
+        material = GetComponent<Renderer>().material;
+    }
+
+    public void AssignColor(Color _color) {
+
+        material.color = _color;
+    }
+}
