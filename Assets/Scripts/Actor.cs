1f00aa57214418d8c710c4daf078f1c8e253c23e
MaxWillekes <max.willekes@student.hku.nl>
2019-09-04 14:55:31 +0200

    re #15, Removed test files and made a first playable of the concept.

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+    public virtual void Death() {
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if(collision.gameObject.name == "Player") {
+            Death();
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour {
+
+    public UnityEvent _keyUpEvent = new UnityEvent();
+    public UnityEvent _keyDownEvent = new UnityEvent();
+    public UnityEvent _keyRightEvent = new UnityEvent();
+    public UnityEvent _keyLeftEvent = new UnityEvent();
+
+    public static InputManager instance { get; private set; }
+
+    void Awake() {
+        if (instance == null) {
+
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Update() {
+
+        if (Input.GetKey("w") && _keyUpEvent != null) {
+            _keyUpEvent.Invoke();
+        }
+
+        if (Input.GetKey("a") && _keyLeftEvent != null) {
+            _keyLeftEvent.Invoke();
+        }
+
+        if (Input.GetKey("s") && _keyDownEvent != null) {
+            _keyDownEvent.Invoke();
+        }
+
+        if (Input.GetKey("d") && _keyRightEvent != null) {
+            _keyRightEvent.Invoke();
+        }
+    }
+}
