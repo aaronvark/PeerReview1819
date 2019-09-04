917fd17cf77d56f6370de8361c7c1a07180177aa
Niels P <niels.poelder@gmail.com>
2019-09-04 14:18:41 +0200

    Added Scripts and documentation to Niels's Asteroids Twist

---
+++ Niels_AsteroidsTwist/Scripts/ScoreScripts/ScoreDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+
+    void Start() {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update() {
+        scoreText.text = "Score: " + ScoreManager.Instance.getPoints().ToString();
+    }
+}
