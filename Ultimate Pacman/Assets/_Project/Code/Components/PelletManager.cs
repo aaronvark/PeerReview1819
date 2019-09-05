e645a4e53c316743e28e1aaaa85a540d27d39ea4
Casey Hofland <hofland.casey@gmail.com>
2019-09-05 13:59:19 +0200

    Added 2 new Managers (Score & Pellet) and added a general movement2D script

---
+++ Ultimate Pacman/Assets/_Project/Code/Components/PelletManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletManager : Singleton<PelletManager>
+{
+    [SerializeField]
+    private string nextScene = "";
+
+    private void Start()
+    {
+        CheckPellets();
+    }
+
+    public void CheckPellets()
+    {
+        if (Pellet.PelletCount == 0)
+        {
+            SceneHandler.instance.LoadScene(nextScene);
+        }
+    }
+}
