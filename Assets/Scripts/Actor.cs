9a7406185c620cbe5d4addaf72c304bc4bd9ef58
MaxWillekes <max.willekes@student.hku.nl>
2019-09-06 16:38:14 +0200

    re #15 Feedback verwerkt, nieuwe code convention toegapast in alle scripts en wat aanpassingen gedaan. OnCollisionStay2D uit Actor gehaald en in Enemy gezet. InputManager van een class naar een interface verandert. GameManager aangemaakt. Player movement verandert van 4 naar 1 void.

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Actor : MonoBehaviour {
-
-    public virtual void Death() {
-    }
-
-    private void OnCollisionStay2D(Collision2D collision) {
-        if(collision.gameObject.name == "Player") {
-            Death();
-        }
+public class Actor : MonoBehaviour
+{
+    public virtual void Death()
+    {
     }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,42 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class InputManager : MonoBehaviour {
+public delegate void onKeyPressedUp(Vector2 movement);
+public delegate void onKeyPressedDown(Vector2 movement);
+public delegate void onKeyPressedLeft(Vector2 movement);
+public delegate void onKeyPressedRight(Vector2 movement);
 
-    public UnityEvent _keyUpEvent = new UnityEvent();
-    public UnityEvent _keyDownEvent = new UnityEvent();
-    public UnityEvent _keyRightEvent = new UnityEvent();
-    public UnityEvent _keyLeftEvent = new UnityEvent();
-
-    public static InputManager instance { get; private set; }
-
-    void Awake() {
-        if (instance == null) {
-
-            instance = this;
-
-            DontDestroyOnLoad(gameObject);
-        } else {
-            Destroy(gameObject);
-        }
-    }
-
-    public void Update() {
-
-        if (Input.GetKey("w") && _keyUpEvent != null) {
-            _keyUpEvent.Invoke();
-        }
-
-        if (Input.GetKey("a") && _keyLeftEvent != null) {
-            _keyLeftEvent.Invoke();
-        }
-
-        if (Input.GetKey("s") && _keyDownEvent != null) {
-            _keyDownEvent.Invoke();
-        }
-
-        if (Input.GetKey("d") && _keyRightEvent != null) {
-            _keyRightEvent.Invoke();
-        }
-    }
+public interface InputManager
+{
+    event onKeyPressedUp _onKeyPressedUpEvent;
+    event onKeyPressedDown _onKeyPressedDownEvent;
+    event onKeyPressedLeft _onKeyPressedLeftEvent;
+    event onKeyPressedRight _onKeyPressedRightEvent;
 }
