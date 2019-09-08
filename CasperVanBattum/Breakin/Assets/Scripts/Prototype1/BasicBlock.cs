e743d6ce9c55a6926767679754548c55533d6653
Casper van Battum <cvanbattum@gmail.com>
2019-09-08 14:22:45 +0200

    Move files again, use Prototype1 namespace

---
+++ CasperVanBattum/Breakin/Assets/Scripts/Prototype1/BasicBlock.cs
@@ -0,0 +1,18 @@
+﻿namespace Prototype1
+{
+	public class BasicBlock : Block
+	{
+		private int strength = 1;
+
+		protected override void OnHit()
+		{
+			strength--;
+
+			// Destroy the gameobject when this block has no more hitpoints
+			if (strength <= 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
