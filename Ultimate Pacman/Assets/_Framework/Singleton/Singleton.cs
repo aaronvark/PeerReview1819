7b008fd3ae1b8bb9d0c3bed52d6cc88779d5bd79
Casey Hofland <hofland.casey@gmail.com>
2019-09-10 01:18:35 +0200

    Simplified 1 line in the Singleton

--- Ultimate Pacman/Assets/_Framework/Singleton/Singleton.cs
+++ Ultimate Pacman/Assets/_Framework/Singleton/Singleton.cs
@@ -29,7 +29,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             {
                 var singletonObject = new GameObject();
                 T t = singletonObject.AddComponent<T>();
-                DestroyImmediate(singletonObject, false);
+                DestroyImmediate(singletonObject);
                 return t;
             }
             else if (destroyed)
