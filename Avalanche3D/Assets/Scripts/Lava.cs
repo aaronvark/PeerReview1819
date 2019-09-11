deb878dd4467677c062881a045e24d0ec7efca0a
reinier303 <30725987+reinier303@users.noreply.github.com>
2019-09-11 11:03:27 +0200

    Lava, DamagableEntity, IDamagable
    Added IDamagable interface to implement in DamagableEntity.
    Lava makes use of IDamagable to detect wether it can Damage the entity

---
+++ Avalanche3D/Assets/Scripts/Lava.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    public float DamageGiven;
+    public float DamageInterval;
+
+    bool CanDamage;
+
+    private void Awake()
+    {
+        CanDamage = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamagable damageTaker = other.GetComponent<IDamagable>();
+        if(damageTaker != null)
+        {
+            StartCoroutine(Damage(damageTaker));
+        }
+    }
+
+    IEnumerator Damage(IDamagable damageTaker)
+    {
+        if(CanDamage)
+        {
+            damageTaker.TakeDamage(DamageGiven);
+            CanDamage = false;
+            yield return new WaitForSeconds(DamageInterval);
+            CanDamage = true;         
+        }
+        else
+        {
+            yield break;
+        }
+    }
+}
