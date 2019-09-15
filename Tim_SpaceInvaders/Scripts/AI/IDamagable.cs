4a7f34fdeca175636a08ece411d811317d3824fe
Tim Peeters <tim.peeters12345@gmail.com>
2019-09-15 12:15:24 +0200

    re #46, more ui stuff

--- Tim_SpaceInvaders/Scripts/AI/IDamagable.cs
+++ Tim_SpaceInvaders/Scripts/AI/IDamagable.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public interface IDamagable
 {
+<<<<<<< HEAD:Tim_SpaceInvaders/Scripts/AI/IDamagable.cs
     void Damage(int damage);
+=======
+    void Damage();
+>>>>>>> 66d911c53c2ed83a918dfb6cdcc3be3dc2d6b19e:Bomberman 3D/Bomberman 3D/Assets/Scripts/IDamagable.cs
     void Die();
 }
