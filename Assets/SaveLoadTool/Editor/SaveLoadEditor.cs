a93c115f99bd7165f567c0c6b9dc307f2a78dd31
Ruben Hooijer <loljoch@gmail.com>
2019-11-20 14:40:30 +0100

    #73 Save & load tool prototype, ben nu bezig met het werkend maken dat je verschillende componenten kan opslaan.

---
+++ Assets/SaveLoadTool/Editor/SaveLoadEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Common.SaveLoadSystem
+{
+
+    [InitializeOnLoad]
+    static class MyHierarchyIcons
+    {
+        static Texture2D texturePanel;
+        static float hierarchyWindowWidth;
+
+        static MyHierarchyIcons()
+        {
+            // Init
+            texturePanel = AssetDatabase.LoadAssetAtPath("Assets/SaveLoadTool/save_icon.png", typeof(Texture2D)) as Texture2D;
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
+        }
+
+        static void HierarchyItemCB(int instanceID, Rect selectionRect)
+        {
+            GameObject go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if (go != null)
+            {
+                if (SaveLoadSystem.IsNodeExisting(go.name))
+                {
+                    // place the icon to the right of the list:
+                    Rect r = new Rect(selectionRect);
+                    r.x = hierarchyWindowWidth;
+                    r.width = 20;
+
+                    GUI.Label(r, texturePanel);
+                }
+            } else
+            {
+                hierarchyWindowWidth = selectionRect.width;
+            }
+
+
+        }
+    }
+
+    static class SaveCommand
+    {
+        [MenuItem("GameObject/Saveable", false, 17)]
+        static void Init(MenuCommand command)
+        {
+            GameObject go = (GameObject)command.context;
+
+            if (!SaveLoadSystem.IsNodeExisting(go.name))
+            {
+                SaveLoadSystem.AddToSave(go.transform);
+            } else
+            {
+                //TODO delete save from XML
+            }
+        }
+    }
+}
---
+++ Assets/SaveLoadTool/Scripts/SaveLoadSystem.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System.Xml;
+
+namespace Common.SaveLoadSystem
+{
+    [InitializeOnLoad]
+    public class SaveLoadSystem
+    {
+        private static XmlDocument xmlDocument = new XmlDocument();
+        
+        static SaveLoadSystem()
+        {
+            try
+            {
+                xmlDocument.Load("Save.xml");
+            } catch (FileNotFoundException)
+            {
+                CreateNewFile();
+            }
+
+            string[] yeet = Directory.GetFiles(Application.dataPath, "SaveLoadSystem.cs", SearchOption.AllDirectories);
+            yeet[0] = yeet[0].Replace(Application.dataPath.Replace('\\', '/'), "");
+        }
+
+        public static bool IsNodeExisting(string objectName)
+        {
+            foreach (XmlNode node in xmlDocument.DocumentElement.ChildNodes)
+            {
+                if (node.Name == ConvertStringTo.XMLFormat(objectName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static XmlNode GetXMLNode(string name)
+        {
+            foreach (XmlNode node in xmlDocument.DocumentElement.ChildNodes)
+            {
+                if (node.Name == ConvertStringTo.XMLFormat(name))
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        public static void AddToSave(Transform obj)
+        {
+            XmlNode xmlNode = null;
+            if (IsNodeExisting(obj.name))
+            {
+                xmlNode = xmlDocument.CreateElement(ConvertStringTo.XMLFormat(obj.name));
+                xmlDocument.DocumentElement.AppendChild(xmlNode);
+            } else
+            {
+                GetXMLNode(ConvertStringTo.XMLFormat(obj.name));
+            }
+
+            XmlNode positionNode = xmlDocument.CreateElement("position");
+            positionNode.InnerText = obj.position.ToString();
+            xmlNode.AppendChild(positionNode);
+
+            xmlDocument.Save("Save.xml");
+        }
+
+        private static void CreateNewFile()
+        {
+            xmlDocument.AppendChild(xmlDocument.CreateElement("Transforms"));
+            xmlDocument.Save("Save.xml");
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        public static void LoadList()
+        {
+            foreach (XmlNode node in xmlDocument.DocumentElement.ChildNodes)
+            {
+                GameObject newObj = new GameObject(node.Name.Replace('_', ' '));
+                newObj.transform.position = ConvertStringTo.Vector3(node.SelectSingleNode("position").InnerText);
+            }
+        }
+    }
+}
